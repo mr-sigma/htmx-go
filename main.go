643837f301799cae64377ddbe20dcd04112c7af9
Chris Cadden <cadden.cs@gmail.com>
2024-01-01 09:33:24 -0500

    feat: add /contacts route

--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 )
 
 func main() {
-  component := hello("John")
-  http.Handle("/", templ.Handler(component))
+  component := hello("world")
+
+  http.HandleFunc("/", indexHandler)
+  http.Handle("/contacts", templ.Handler(component))
 
   fmt.Println("Listening on :3000")
   http.ListenAndServe(":3000", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  http.Redirect(w, r, "/contacts", http.StatusFound)
+  return
 }
